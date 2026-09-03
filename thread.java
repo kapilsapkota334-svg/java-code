521759452247fb73cf408e1ca25dfcf48fdb2c57
kapilsapkota334-svg <kapilsapkota334@gmail.com>
2026-09-03 22:48:42 +0545

    Add my new practice code

---
+++ thread.java
@@ -0,0 +1,21 @@
+public class thread extends Thread {
+
+    @Override
+    public void run() {
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        thread t1 = new thread();
+
+        t1.start();
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
